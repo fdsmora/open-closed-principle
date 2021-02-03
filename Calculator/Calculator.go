ab7a51e926760780703870bfbfb3d3d7865cec6c
Fausto Salazar <fsalazar@brightcove.com>
2021-02-03 08:03:30 -0600

    Working example

---
+++ Calculator/Calculator.go
@@ -0,0 +1,17 @@
+package Calculator
+
+type Calculator struct {
+	result float64
+}
+
+func (calc *Calculator) Sum(a float64, b float64) {
+	calc.result = a + b
+}
+
+func (calc *Calculator) Substract(a float64, b float64) {
+	calc.result = a - b
+}
+
+func (calc *Calculator) Result() float64 {
+	return calc.result
+}
